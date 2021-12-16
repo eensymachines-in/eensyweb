6a5be877468a44454c8c1e1f94a8d2d6be7ffe02
Kneerunjun <kneerunjun@gmail.com>
2021-12-16 18:45:28 +0530

    code #6

---
+++ README.md
@@ -0,0 +1,5 @@
+
+#### Front facing website for Eensymachines :
+----
+
+For customers visiting us on the web we need a landing website. 
--- eensywebproxy/proxy.go
+++ eensywebproxy/proxy.go
@@ -26,12 +26,46 @@ type blogData struct {
 	Data []blogMeta `json:"d"`
 }
 
+// getBaseUrl :Nginx will inject headers before passing the request to the proxy here.
+// We get the X-Host from the header and then form the the baseurl
+func getBaseUrl(c *gin.Context) (string, error) {
+	baseIP := c.Request.Header.Get("X-Host")
+	if baseIP == "" {
+		return "", fmt.Errorf("invalid X-Host in the header: expecting host IP/domain in it")
+	}
+	return fmt.Sprintf("http://%s", baseIP), nil
+}
+
 // sendIndexHtml : common function that sends out index.html with basic page information
 func sendIndexHtml(c *gin.Context) {
+
+	absBaseUrl, _ := getBaseUrl(c)
 	c.HTML(http.StatusOK, "index.html", gin.H{
 		"pageTitle": "Eensymachines",
+		"ogImage":   fmt.Sprintf("%s/images/circuit06.jpg", absBaseUrl),
+		"ogUrl":     absBaseUrl,
+		"ogTitle":   "EensyMachines",
+		"ogDesc":    "Internet of things company",
 	})
-	return
+}
+
+// dispatchError : will send out the error page when called
+// depending on the httpstatus code this picks the correct page and dispatches the error message
+func dispatchError(code int, c *gin.Context, e string) {
+	title := "Unknown error"
+	if code == 400 {
+		title = "Bad request"
+	} else if code == 404 {
+		title = "Not found"
+	} else if code == 500 {
+		title = "Internal server error"
+	}
+	c.HTML(code, fmt.Sprintf("%d.html", code), gin.H{
+		"pageTitle":  "Eensymachines",
+		"imagePath":  fmt.Sprintf("/images/%d.jpg", code),
+		"errTitle":   title,
+		"errMessage": e,
+	})
 }
 func main() {
 	gin.SetMode(gin.DebugMode)
@@ -39,35 +73,31 @@ func main() {
 	r.LoadHTMLGlob(fmt.Sprintf("%s/pages/*", statics))
 	// r.LoadHTMLGlob("/usr/src/eensy/web/views/*")
 	r.GET("/", sendIndexHtml)
-	r.GET("/blogs", sendIndexHtml)
+
 	r.GET("/blogs/:bid", func(c *gin.Context) {
-		jsonFile, err := os.Open(fmt.Sprintf("%s/blogs.json", statics))
+		jsonFile, err := os.Open(fmt.Sprintf("%s/data/blogs.json", statics))
 		bid := c.Param("bid")
 		if err != nil {
+			fmt.Println(err)
 			// failed to open the data file
 			// sending std. index.html without any modification on the meta tags
-			c.HTML(http.StatusOK, "index.html", gin.H{
-				"pageTitle": "Eensymachines",
-			})
+			dispatchError(500, c, "We arent able to find the blog information right now. Wait for admin to fix this.")
 			return
 		}
 		defer jsonFile.Close() // the file is closed on exit
 		byteValue, _ := ioutil.ReadAll(jsonFile)
 		data := blogData{[]blogMeta{}}
 		if json.Unmarshal(byteValue, &data) != nil {
-			c.HTML(http.StatusInternalServerError, "500.html", gin.H{
-				"pageTitle": "Eensymachines",
-			})
+			dispatchError(500, c, "Failed to read blog content. Something on the server side isnt quite right. Wait for admin to fix this")
 			return
 		}
-		baseIP := c.Request.Header.Get("X-Host")
-		fmt.Printf("Header values %v\n", c.Request.Header)
-		absBaseUrl := fmt.Sprintf("http://%s", baseIP)
+		fmt.Printf("Data from json file..%v", data)
+		absBaseUrl, err := getBaseUrl(c)
 
-		if baseIP == "" {
-			c.HTML(http.StatusBadRequest, "400.html", gin.H{
-				"pageTitle": "Eensymachines",
-			})
+		if err != nil {
+			// this happens when the nginx server url wasnt read back into the proxy
+			// nginx server IP/domain is passed into the proxy as a part of a header
+			dispatchError(400, c, "Something not right about the request being sent. Wait for an admin to fix this")
 			return
 		}
 		for _, m := range data.Data {
@@ -85,14 +115,22 @@ func main() {
 			}
 		}
 		fmt.Printf("Blog with the id wasnt found %s\n", bid)
-		c.HTML(http.StatusBadRequest, "400.html", gin.H{
-			"pageTitle":  "Eensymachines",
-			"errMessage": "Blog with id wasnt found- it may have been removed!",
-		})
+		dispatchError(404, c, "Blog content was moved from here, perhaps retired. Cannot say when shall it be back.")
 
 	})
-	r.GET("/products", sendIndexHtml)
+	r.GET("/blogs", sendIndexHtml)
 	r.GET("/products/:bid", sendIndexHtml)
+	r.GET("/products", sendIndexHtml)
 	r.GET("/about", sendIndexHtml)
+	r.GET("/error", func(c *gin.Context) {
+		// Sends out an error page
+		// this route is only for testing purposes as the error page is essentially sent from other handlers
+		c.HTML(http.StatusBadRequest, "error.html", gin.H{
+			"pageTitle":  "Eensymachines",
+			"imagePath":  "/images/400.jpg",
+			"errTitle":   "Not found",
+			"errMessage": "Failed to get resource at the desired location.",
+		})
+	})
 	log.Fatal(r.Run(":8080"))
 }
